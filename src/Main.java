32f286a1cf9a7bafb31c45b3aa5c137e19c061a8
Ram Gundelli <rgundelli@gmail.com>
2018-10-28 09:32:04 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("A simple text editor for validating XML, JSON, and YML files.");
+        System.out.println("Find Devitor zip file in the lib folder, unzip and copy over the executable file.");
+    }
+}
